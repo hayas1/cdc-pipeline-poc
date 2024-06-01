0c70b3228e03db941e97261461911a4e1a036656
hayas1 <h4ystack@gmail.com>
2024-06-01 11:30:05 +0000

    order

---
+++ mysql/initdb.d/sql0002_create_database.sql
@@ -0,0 +1,2 @@
+DROP SCHEMA IF EXISTS sample_schema;
+CREATE SCHEMA sample_schema;
---
+++ mysql/initdb.d/sql0003_create_employee_table.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS sample_schema.employee;
+CREATE TABLE sample_schema.employee
+(
+  id           INT(10),
+  name     VARCHAR(40)
+);
+
+
+INSERT INTO sample_schema.employee (id, name) VALUES (1, "Hayashi");
+INSERT INTO sample_schema.employee (id, name) VALUES (2, "Hiyashi");
+INSERT INTO sample_schema.employee (id, name) VALUES (3, "Huyashi");
+INSERT INTO sample_schema.employee (id, name) VALUES (4, "Heyashi");
+INSERT INTO sample_schema.employee (id, name) VALUES (5, "Hoyashi");
