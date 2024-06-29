1d2008a92f2adeb48c0f71cd3fdbf26d9ccb1f16
hayas1 <h4ystack@gmail.com>
2024-06-29 01:05:07 +0000

    wait

--- test.sh
+++ test.sh
@@ -6,14 +6,15 @@ docker-compose -f cdc-compose.yaml up --build -d
 until docker-compose -f cdc-compose.yaml exec -T kafka kafka-topics.sh --list --bootstrap-server=localhost:9092; do
     sleep 1
 done
+until curl -i http://localhost:8083/connectors | grep "200 OK"; do
+    sleep 1
+done
+
 topics=$(docker-compose -f cdc-compose.yaml exec -T kafka kafka-topics.sh --list --bootstrap-server=localhost:9092)
 echo "$topics" | grep debezium_configs
 echo "$topics" | grep debezium_offsets
 echo "$topics" | grep debezium_statuses
 
-until curl -i http://localhost:8083/connectors | grep "200 OK"; do
-    sleep 1
-done
 curl http://localhost:8083/connectors -H 'Content-Type: application/json' -X POST -d @debezium-mysql-connector.json
 
 topics=$(docker-compose -f cdc-compose.yaml exec -T kafka kafka-topics.sh --list --bootstrap-server=localhost:9092)
