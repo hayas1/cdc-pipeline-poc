7a6844a202cd78d81760e9f344afe372b4257465
hayas1 <h4ystack@gmail.com>
2024-06-29 00:26:23 +0000

    test.sh

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+docker-compose -f cdc-compose.yaml up -d
+
+topics=$(docker-compose -f cdc-compose.yaml exec kafka kafka-topics.sh --list --bootstrap-server=localhost:9092)
+echo "$topics" | grep debezium_configs
+echo "$topics" | grep debezium_offsets
+echo "$topics" | grep debezium_statuses
+
+curl http://localhost:8083/connectors -H 'Content-Type: application/json' -X POST -d @debezium-mysql-connector.json
+
+topics=$(docker-compose -f cdc-compose.yaml exec kafka kafka-topics.sh --list --bootstrap-server=localhost:9092)
+echo "$topics" | grep mysql_hostname
+echo "$topics" | grep mysql_hostname.sample_schema.employee
+echo "$topics" | grep schemahistory.sample_schema
+
+docker-compose -f cdc-compose.yaml down
